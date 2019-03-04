803990b1dac0212c850c598360119c22f60472c2
Ronan Macken <ronan.macken@mydit.ie>
2019-03-04 08:25:33 +0000

    image

--- mysite/skate/urls.py
+++ mysite/skate/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import SpotListView,SpotDetailView
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'skate'
 urlpatterns = [
     path('', SpotListView.as_view(), name='index'),
@@ -13,4 +14,4 @@ urlpatterns = [
     
     
     
-]
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
